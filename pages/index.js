07f60e158d36576024ffe325f7f6459cfa6b8ff3
Lne-Fox <Adityajha2800@gmail.com>
2021-08-07 16:46:15 +0530

    day two build

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
@@ -14,44 +15,44 @@ export default function Home({ exploreData, cardData }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     
+
       <Header />
 
       <Banner />
 
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
-        <section className="pt-6">
-          <h2 className="text-3xl font-semibold pb-5" >Explore Nearby</h2>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 shadow-md">
+        <section className="pt-6 drop-shadow">
+          <h2 className="text-3xl font-semibold pb-5 " >Explore Nearby</h2>
 
           {/* Pull some data from a server - API endpoints */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {exploreData?.map(({img, distance, location}) => (
-            <SmallCard key={img} img={img} distance={distance} location={location}  />
-          ))}
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard key={img} img={img} distance={distance} location={location} />
+            ))}
           </div>
-          
+
         </section>
 
-        <section>
+        <section  >
           <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
 
           <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
-          {cardData?.map(({img, title}) => (
-            <MediumCard key={img} img={img} title={title} />
-          ))}
+            {cardData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
           </div>
 
-          
+
         </section>
 
-        <LargeCard 
-        img='https://links.papareact.com/4cj'
-        title ='The Greatest Outdoors'
-        description='Wishlists curated by Airbnb'
-        buttonText='Get Inspired'
+        <LargeCard
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          description='Wishlists curated by Airbnb'
+          buttonText='Get Inspired'
         />
       </main>
-      
+
       <Footer />
     </div>
   )
